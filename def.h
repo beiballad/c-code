565e540477ec32bb06f6d2ad4192e9db235d8549
beiballad <140420664+beiballad@users.noreply.github.com>
2023-07-25 23:25:05 +0800

    Add files via upload

---
+++ def.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef _DEF_H_
+#define _DEF_H_
+
+#define MaxSize 10
+
+struct SqList//Ë³Ðò±í
+{
+	int data[MaxSize];
+	int length = 0;
+};
+
+
+#endif
---
+++ test8.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef _TEST8_H_
+#define _TEST8_H_
+
+extern bool reverse(int A[], int r, int l, int size);//逆置数组A中下标r到l的所有元素
+
+
+#endif
