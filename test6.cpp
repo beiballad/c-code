280875f0a6319e27442f9699610a767b476d4f18
beiballad <140420664+beiballad@users.noreply.github.com>
2023-07-27 14:20:11 +0800

    Update test6.cpp

--- test6.cpp
+++ test6.cpp
@@ -4,12 +4,12 @@
 #include "def.h"
 
 
-bool sort(LinkList L)
+bool sort(LinkList L)//使带头结点的单链表递增有序
 {
 	if (L->next == NULL)
-		return false;
+		return false;//判空，直接返回
 	LNode* p = L->next, * pre = L, * q = p->next, * qnext = q->next;
-	p->next = NULL;
+	p->next = NULL;//将首项的后继指向空指针，以构成只有一个元素的有序链表
 	while (q != NULL)
 	{
 		qnext = q->next;
@@ -27,7 +27,7 @@ bool sort(LinkList L)
 				q->next = p;
 				q = qnext;
 				break;
-			}
+			}//插入排序法
 	}
 	return true;
 }
